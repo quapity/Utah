8818640bab0e4aa26701c416d6a978ef36eae8a6
lisa <quapity@gmail.com>
2018-02-01 16:12:17 -0700

    function pipeline update

--- Utah_LSTM.py
+++ Utah_LSTM.py
@@ -6,16 +6,17 @@ Created on Fri Jan 26 08:26:12 2018
 @author: lisa
 """
 import pandas as pd
-import pickle
 import numpy as np
 
 import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap
+import adjustText
 
 plt.switch_backend("nbagg")
 plt.style.use('seaborn-white')
 plt.rcParams.update({'font.size': 18})
 plt.rcParams['savefig.dpi'] = 80
+plt.rcParams['figure.figsize']=14,14
 from obspy.clients.fdsn import Client
 client = Client("IRIS")
 
@@ -27,6 +28,7 @@ from obspy.core import UTCDateTime
 from keras.models import load_model
 model = load_model('models/LSTM_40_1445.h5')
 
+#%matplotlib inline
 #%%
 def rotater(tr,inv,evlo,evla):
     lat= inv[0][0].latitude
@@ -55,6 +57,19 @@ def scale_transform(x):
 def scale_transform_view(x):
     x = np.vstack(np.nan_to_num(x/np.max(x)))
     return 1+(-np.log(x+.0002)/np.median(np.log(x+.0002)))
+
+def plot_txt(x,y,text):
+    
+    font = {'family': 'Arial',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 12,
+        }
+    texts = []
+    for xt, yt, s in zip(x, y, text):
+        texts.append(plt.text(xt, yt, s,bbox=dict(facecolor='white', edgecolor='white',
+                     boxstyle='round,pad=.2', alpha = .4),fontdict = font))
+    return texts
 
 def parse_dbselect(filename = 'dbcat'):
     ddict ={'id':[],'type':[],'station':[],'net':[],'chan':[],'impulsive':[],
@@ -204,22 +219,160 @@ def preproc_data(filename ='out_from_parse_dbselect', to_file=False, num_events=
 #%%
 
  
-pred = model.predict(np.array([scale_transform(x) for x in pdf['x']]))
-def eval_pred(method='viz'):
-    '''look at spectrograms type = 'viz', or look a table'''
-    if method == 'viz':
-        m = Basemap(llcrnrlat=-35,urcrnrlat=43,llcrnrlon=-115.5,urcrnrlon=-106,resolution='h',epsg = 32142)
-        m.drawstates()
-        for each in range(len(pdf)):
-            plt.subplot(211)
-            title = 'ID: ' + str(pdf.iloc[each]['id'])+' Label: '+ str(pdf.iloc[each]['type'])+ ' Prediction: ' + str(pred[each])
-            plt.imshow(scale_transform_view(pdf.iloc[each]['x']),origin='lower',
-               cmap = 'RdGy_r',aspect='auto',interpolation='gaussian');plt.colorbar()
-            plt.title(title)
-            plt.subplot(212)
-            x,y = m(pdf.iloc[each]['evlo'],pdf.iloc[each]['evla'])
-            plt.scatter(x,y,s=100)
-            plt.show()
+#pred = model.predict(np.array([scale_transform(x) for x in pdf['x']]))
+#def eval_pred(method='viz'):
+#    '''look at spectrograms type = 'viz', or look a table'''
+#    if method == 'viz':
+#        m = Basemap(llcrnrlat=-35,urcrnrlat=43,llcrnrlon=-115.5,urcrnrlon=-106,resolution='h',epsg = 32142)
+#        m.drawstates()
+#        for each in range(len(pdf)):
+#            plt.subplot(211)
+#            title = 'ID: ' + str(pdf.iloc[each]['id'])+' Label: '+ str(pdf.iloc[each]['type'])+ ' Prediction: ' + str(pred[each])
+#            plt.imshow(scale_transform_view(pdf.iloc[each]['x']),origin='lower',
+#               cmap = 'RdGy_r',aspect='auto',interpolation='gaussian');plt.colorbar()
+#            plt.title(title)
+#            plt.subplot(212)
+#            x,y = m(pdf.iloc[each]['evlo'],pdf.iloc[each]['evla'])
+#            plt.scatter(x,y,s=100)
+#            plt.show()
 
 
+#%%
 
+
+def plt_events(data):
+    fig= plt.figure()
+    latmin = min(data['stla']) -.1
+    latmax = max(data['stla'])+.1
+    lomin = min(data['stlo']) -.1
+    lomax = max(data['stlo'])+.1
+    m = Basemap(llcrnrlat=latmin,urcrnrlat=latmax,llcrnrlon=lomin,
+        urcrnrlon=lomax,resolution='h',epsg = 32142)
+    #m.fillcontinents(color='grey',lake_color='black',zorder=0)
+    m.arcgisimage(service='World_Shaded_Relief', xpixels = 1500, verbose= True,zorder=0)
+    m.drawstates()
+    m.drawcountries()
+    #m.drawcoastlines()
+    
+    parallels = np.arange(35,47,1)
+    # labels = [left,right,top,bottom]
+    m.drawparallels(parallels,linewidth=0.1,labels=[True,False,False,False])
+    meridians = np.arange(-115,-105,1)
+    m.drawmeridians(meridians,linewidth = 0.1,labels=[False,False,False,True])
+    for event in list(set(data.id)):
+        idx = np.where(np.array(data['id']) == event)[0]
+        #predictions = np.array(model.predict(np.array([scale_transform(x) for x in testcat['sx'][idx]])) )   
+        lat=data.iloc[idx[0]].evla
+        lon = data.iloc[idx[0]].evlo
+        lo,la = m(lon,lat)
+        slat,slo,stcol = [],[],[]
+        for i in idx:
+            if data.iloc[i]['lbl'] == 0:
+                color = 'b'
+        
+            else:
+                color= 'r'
+            if data.iloc[i]['lbl'] == data.iloc[i]['preds']:
+                   stcol ='white'
+            else:
+                stcol = 'black'
+    
+            x,y = m(data.iloc[i]['stlo'],data.iloc[i]['stla'])
+            plt.plot([lo,x],[la,y],c=color,linewidth=.4)
+            plt.scatter(x,y,facecolor=stcol,edgecolor='black',s=70,alpha =.2,zorder=500000)
+        #plt.scatter(x,y,facecolor=color,edgecolor=color,alpha=.6,s= 30)
+        plt.scatter(lo,la,facecolor=color,edgecolor=color,s=20, alpha = .6)
+    
+    slat,slo =[],[]
+    for sta in list(set(data.id)):
+        if sta > 0:
+            idx = np.where(np.array(data['id']) == int(sta))[0]
+            slat.append(data.iloc[idx[0]]['evla'])
+            slo.append(data.iloc[idx[0]]['evlo'])
+        else:
+            idx = np.where(np.array(data['station']) == sta)[0]
+            slat.append(data.iloc[idx[0]]['stla'])
+            slo.append(data.iloc[idx[0]]['stlo'])
+    x,y = m(slo,slat)
+    #plt.scatter(x,y,facecolor='white',edgecolor='cornflowerblue',s=70, zorder = 50000)
+    text= plot_txt(x,y,list(set(data.id)))
+    adjustText.adjust_text(text, arrowprops=dict(arrowstyle="-", color='black', lw=0.1),
+            autoalign='y')
+        
+    #plt.savefig('intro_map3.eps',bbox_inches='tight', transparent =True)
+    return fig    
+
+def make_stats(data,filename='stats_dict.html'):
+    statsdict ={'id':[],'type':[],'stations':[],'datetime':[],'mag':[],
+                'evla':[],'evlo':[],'depth':[],'class_prediction':[],'how_sure':[]}
+    for ev in list(set(data.id)):
+        idx = np.where(np.array(data.id) == ev)[0]
+        statsdict['id'].append(ev)
+        statsdict['type'].append(data.iloc[idx[0]]['lbl'])
+        statsdict['stations'].append(np.array(data.iloc[idx]['station']))
+        statsdict['evla'].append(data.iloc[idx[0]]['evla'])
+        statsdict['evlo'].append(data.iloc[idx[0]]['evlo'])
+        statsdict['datetime'].append(data.iloc[idx[0]]['datetime'])
+        statsdict['mag'].append(data.iloc[idx[0]]['mag'])
+        statsdict['depth'].append(data.iloc[idx[0]]['depth'])
+        statsdict['class_prediction'].append(np.argmax(np.sum(data.iloc[idx]['predictions'])))
+        statsdict['how_sure'].append(max(np.sum(data.iloc[idx]['predictions']))/len(data.iloc[idx]))
+        pd.DataFrame(statsdict).to_html(filename)
+    return pd.DataFrame(statsdict)
+#%%
+#fig= plt.figure()
+#m = Basemap(llcrnrlat=35.5,urcrnrlat=45.1,llcrnrlon=-115,
+#        urcrnrlon=-107.5,resolution='h',epsg = 32142)
+##m.fillcontinents(color='grey',lake_color='black',zorder=0)
+#m.arcgisimage(service='World_Shaded_Relief', xpixels = 1500, verbose= True,zorder=0)
+#m.drawstates()
+#m.drawcountries()
+##m.drawcoastlines()
+#
+#parallels = np.arange(35,47,1)
+## labels = [left,right,top,bottom]
+#m.drawparallels(parallels,linewidth=0.1,labels=[True,False,False,False])
+#meridians = np.arange(-115,-105,1)
+#m.drawmeridians(meridians,linewidth = 0.1,labels=[False,False,False,True])
+#for event in list(set(data.id)):
+#    idx = np.where(np.array(data['id']) == event)[0]
+#        #predictions = np.array(model.predict(np.array([scale_transform(x) for x in testcat['sx'][idx]])) )   
+#    lat=data.iloc[idx[0]].evla
+#    lon = data.iloc[idx[0]].evlo
+#    lo,la = m(lon,lat)
+#    slat,slo,stcol = [],[],[]
+#    for i in idx:
+#        if data.iloc[i]['lbl'] == 0:
+#            color = 'b'
+#        
+#        else:
+#            color= 'r'
+#        if data.iloc[i]['lbl'] == data.iloc[i]['preds']:
+#            stcol ='white'
+#        else:
+#            stcol = 'black'
+#
+#        x,y = m(data.iloc[i]['stlo'],data.iloc[i]['stla'])
+#        plt.plot([lo,x],[la,y],c=color,linewidth=.4)
+#        plt.scatter(x,y,facecolor=stcol,edgecolor=color,s=70,alpha =.2,zorder=500000)
+#        #plt.scatter(x,y,facecolor=color,edgecolor=color,alpha=.6,s= 30)
+#    plt.scatter(lo,la,facecolor=color,edgecolor=color,s=20, alpha = .6)
+#
+#slat,slo =[],[]
+#for sta in np.concatenate((list(set(data.station)),list(set(data.id)))):
+#    if sta[0] == '6':
+#        idx = np.where(np.array(data['id']) == int(sta))[0]
+#        slat.append(data.iloc[idx[0]]['evla'])
+#        slo.append(data.iloc[idx[0]]['evlo'])
+#    else:
+#        idx = np.where(np.array(data['station']) == sta)[0]
+#        slat.append(data.iloc[idx[0]]['stla'])
+#        slo.append(data.iloc[idx[0]]['stlo'])
+#x,y = m(slo,slat)
+##plt.scatter(x,y,facecolor='white',edgecolor='cornflowerblue',s=70, zorder = 50000)
+#text= plot_txt(x,y,np.concatenate((list(set(data.station)),list(set(data.id)))))
+#adjustText.adjust_text(text, arrowprops=dict(arrowstyle="-", color='black', lw=0.1),
+#            autoalign='y')
+#        
+##plt.savefig('intro_map3.eps',bbox_inches='tight', transparent =True)
+#plt.show()
